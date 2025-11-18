e2ad165e212c17997227107522ca83e2011f0944
Naufal-farhan <nefener123@gmail.com>
2025-11-18 19:03:26 +0700

    Modifikasi Percobaan-3

--- Number20.java
+++ Number20.java
@@ -7,6 +7,13 @@ public class Number20 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+        
+        for(int i = 0; i < myNumbers.length; i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for(int i = 0; i < myNumbers.length; i++){
+            System.out.println("Panjang baris ke-"+(i+1)+" : "+myNumbers[i].length);
+        }
     }
     
 }
